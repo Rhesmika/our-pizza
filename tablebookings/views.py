214969c4d584c9d5e31c62d1eb7daa5270987395
Rhesmika <Rhesmika@gmail.com>
2022-11-24 17:12:53 +0000

    link css to html and connect booking urls

--- tablebookings/views.py
+++ tablebookings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Booking
+from django.views import generic
 
-# Create your views here.
+
+class BookingsApproved(generic.ListView):
+    model = Booking
+    queryset = Booking.objects.filter(status=1).order_by("booking_date")
+    template_name = 'bookings.html'
+    paginate_by = 5
