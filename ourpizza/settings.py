7f816332435bca1804d3991aa307686121b11138
Rhesmika <Rhesmika@gmail.com>
2022-11-24 14:03:01 +0000

    update typo on procfile

--- ourpizza/settings.py
+++ ourpizza/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['our-pizza.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ["our-pizza.herokuapp.com", "localhost"]
 
 
 # Application definition
