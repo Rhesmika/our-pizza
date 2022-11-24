21cd12a305ff3afd6f092a83b5b217c68d7b7883
Rhesmika <Rhesmika@gmail.com>
2022-11-24 14:18:06 +0000

    Create table booking model

--- tablebookings/models.py
+++ tablebookings/models.py
@@ -1,4 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+REQUEST_STATUS = ((0, 'Pending'), (1, 'Approved'), (3, 'Denied'))
+
+
+class Booking(models.Model):
+    name = User()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    reference = models.CharField(max_length=50)
+    booking_date = models.DateField()
+    party_of = models.IntegerField(
+        default=2,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+     )
+    status = models.IntegerField(choices=REQUEST_STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
