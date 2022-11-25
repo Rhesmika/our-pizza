000ba6ab4f16159eee5c71fc8aac714f91237b8b
Rhesmika <Rhesmika@gmail.com>
2022-11-25 16:52:47 +0000

    Add user registration to website and add to nav bar

--- ourpizza/urls.py
+++ ourpizza/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tablebookings.urls'), name='tablebookings_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
