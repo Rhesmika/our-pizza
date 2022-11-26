a8946c0f8d608773cc1af30e24e730d36a029ccd
Rhesmika <Rhesmika@gmail.com>
2022-11-26 23:43:20 +0000

    re-link sucess link of new booking to go to bookings page

--- tablebookings/views.py
+++ tablebookings/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Booking
 from django.views import generic
 from datetime import datetime
 from .forms import NewBookingForm
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User
 
 
@@ -17,7 +17,7 @@ class UpcomingBookingsApproved(generic.ListView):
 class NewBooking(CreateView):
     form_class = NewBookingForm
     model = Booking
-    success_url = 'my-bookings'
+    success_url = '/bookings'
     template_name = 'bookings-new.html'
 
     def form_valid(self, form):
@@ -27,5 +27,16 @@ class NewBooking(CreateView):
         self.object.save()
         return super().form_valid(form)
 
-def EditBooking(request, item_id):
-    return render(request, 'edit-booking.html')
+
+class EditBooking(CreateView):
+    form_class = NewBookingForm
+    model = Booking
+    success_url = ''
+    template_name = 'bookings-new.html'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.status = 0
+        self.object.save()
+        return super().form_valid(form)
