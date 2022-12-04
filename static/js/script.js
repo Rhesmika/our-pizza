e827cf0b8b7f1908209c747c592901c9265e96a0
Rhesmika <Rhesmika@gmail.com>
2022-12-04 19:29:58 +0000

    add animation to homepage slogan

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,30 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active");
     }
 })
+
+
+const slogan = document.querySelector("#slogan-text");
+const strSlogan = slogan.textContent;
+const splitSlogan = strSlogan.split("");
+slogan.textContent = "";
+for (let i=0; i < splitSlogan.length; i++){
+    slogan.innerHTML += "<span>" + splitSlogan[i] +"</span>";
+}
+
+let character =0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = slogan.querySelectorAll("span")[character]
+    span.classList.add("reveal");
+    character++;
+    if (character === splitSlogan.length) {
+        complete();
+        return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
