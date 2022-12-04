24c7c3c3bfae61d80e7d9e8fff5346619f64ae26
Rhesmika <Rhesmika@gmail.com>
2022-12-04 21:41:57 +0000

    booking table status converts integers to names pending or approved

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 const toTop = document.querySelector(".scroll-to-top");
 
+/* Scroll to top */
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
         toTop.classList.add("active");
@@ -8,30 +9,54 @@ window.addEventListener("scroll", () => {
     }
 })
 
+// /* Slogan */
+// const slogan = document.querySelector("#slogan-text");
+// const strSlogan = slogan.textContent;
+// const splitSlogan = strSlogan.split("");
+// slogan.textContent = "";
+// for (let i=0; i < splitSlogan.length; i++){
+//     slogan.innerHTML += "<span>" + splitSlogan[i] +"</span>";
+// }
 
-const slogan = document.querySelector("#slogan-text");
-const strSlogan = slogan.textContent;
-const splitSlogan = strSlogan.split("");
-slogan.textContent = "";
-for (let i=0; i < splitSlogan.length; i++){
-    slogan.innerHTML += "<span>" + splitSlogan[i] +"</span>";
-}
+// let character =0;
+// let timer = setInterval(onTick, 50);
 
-let character =0;
-let timer = setInterval(onTick, 50);
+// function onTick(){
+//     const span = slogan.querySelectorAll("span")[character]
+//     span.classList.add("reveal");
+//     character++;
+//     if (character === splitSlogan.length) {
+//         complete();
+//         return;
+//     }
+// }
 
-function onTick(){
-    const span = slogan.querySelectorAll("span")[character]
-    span.classList.add("reveal");
-    character++;
-    if (character === splitSlogan.length) {
-        complete();
-        return;
+// function complete(){
+//     clearInterval(timer);
+//     timer = null;
+// }
+
+
+/* Booking Status Converter */
+
+
+var tableStatus = document.getElementsByClassName('booking-status');
+
+for (var i = 0; i < tableStatus.length; ++i) {
+    console.log(tableStatus[i].innerHTML);
+    if (tableStatus[i].innerHTML == "0"){
+        newStatus = "Pending";
+        updateStatusPending(newStatus);
+    } else if (tableStatus[i].innerHTML == "1"){
+        newStatus = "Approved";
+        updateStatusApproved(newStatus);
     }
 }
 
-function complete(){
-    clearInterval(timer);
-    timer = null;
+function updateStatusPending(newStatus){
+    tableStatus[i].innerHTML = newStatus;
 }
 
+function updateStatusApproved(newStatus){
+    tableStatus[i].innerHTML = newStatus;
+}
